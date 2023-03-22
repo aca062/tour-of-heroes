12c88c6d80d3c1372fc6ce8a03ad76159edd831e
Antonio Jesús <aca062@inlumine.ual.es>
2023-03-22 19:49:22 +0100

    Proyecto completado

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', superPowers: ['Invisibilidad'] },
+  { id: 13, name: 'Bombasto', superPowers: ['Creación de explosiones'] },
+  {
+    id: 14,
+    name: 'Celeritas',
+    superPowers: ['Teletransportación', 'Visión de rayos X'],
+  },
+  { id: 15, name: 'Magneta', superPowers: ['Invulnerabilidad'] },
+  { id: 16, name: 'RubberMan', superPowers: ['Cambiar de forma'] },
+  { id: 17, name: 'Dynama', superPowers: ['Súper velocidad'] },
+  { id: 18, name: 'Dr. IQ', superPowers: ['Telepatía', 'Telequinesis'] },
+  { id: 19, name: 'Magma', superPowers: ['Manipulación del fuego'] },
+  { id: 20, name: 'Tornado', superPowers: ['Volar'] },
+];
